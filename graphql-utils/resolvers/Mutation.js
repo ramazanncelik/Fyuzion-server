30220bc238f0e8279910080d8cce55bbfd747476
Ramazan Çelik <73830412+ramazanncelik@users.noreply.github.com>
2023-03-10 19:04:28 +0300

    Update Mutation.js

--- graphql-utils/resolvers/Mutation.js
+++ graphql-utils/resolvers/Mutation.js
@@ -1,7 +1,7 @@
-import pubSub from '../pubSub'
-import User from "../../models/User";
-import Post from "../../models/Post";
-import Comment from "../../models/Comment";
+import pubSub from '../pubSub.js'
+import User from "../../models/User.js";
+import Post from "../../models/Post.js";
+import Comment from "../../models/Comment.js";
 
 const Mutation = {
     // User
